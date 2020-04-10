13455cc8be81ab166ae3a4575a51dbfbd5df08df
Domenico Medeiros Travalia <dm.travalia@gmail.com>
2020-04-09 22:57:38 -0300

    add services

--- WorkerService_ColetaInformacao/Program.cs
+++ WorkerService_ColetaInformacao/Program.cs
@@ -1,3 +1,7 @@
+using BotCrypto.ColetaInformacao.Application.Services;
+using BotCrypto.ColetaInformacao.Data;
+using BotCrypto.ColetaInformacao.Data.Repository;
+using BotCrypto.ColetaInformacao.Domain;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -15,6 +19,13 @@ namespace WorkerService_ColetaInformacao
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
+
+                    // Catalogo
+                    services.AddScoped<ITickerRepository, TickerRepository>();
+                    services.AddScoped<ITickerService, TickerService>();
+                    services.AddScoped<ITickerAppService, TickerAppService>();
+                    services.AddScoped<ColetaInformacaoContext>();
+
                 });
     }
 }
